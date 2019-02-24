8bba9cfdb5cffe91a16bcf0e4a3bf34fca41c764
Josh <jspri1@gmail.com>
2019-02-24 06:19:52 -0600

    Added better inputs to neat_ff_agent

--- agents/neat_ff_agent.py
+++ agents/neat_ff_agent.py
@@ -42,10 +42,44 @@ class Agent():
         genome_id, genome = self.population_object.get_population()[self.genome_index]
         genome.fitness = game_state["distance_traveled"]
 
+        num_segments = 11
+        inputs = [0] * num_segments
+        sensor_range = 350
+        for i in range(num_segments):
+            
+            segment_top = -(sensor_range / 2.0) + (i * (float(sensor_range) / num_segments))
+            segment_bottom = -(sensor_range / 2.0) + ((i + 1) * (float(sensor_range) / num_segments))
+            segment_center = (segment_top + segment_bottom) / 2.0
+
+            #print(segment_top)
+            #print(segment_bottom)
+            #print(game_state["player_dist_to_floor"])
+            #print(game_state["player_dist_to_ceil"])
+            #print()
+
+            blocked = False
+            if -game_state["player_dist_to_ceil"] > segment_top:
+                blocked = True
+            elif game_state["player_dist_to_floor"] < segment_bottom:
+                blocked = True
+            #elif segment_center < (game_state["player_y"] - game_state["next_gate_block_top"]) and segment_center > (game_state["player_y"] - game_state["next_gate_block_bottom"]):
+                #blocked = True
+            elif (game_state["player_y"] + segment_center) > game_state["next_gate_block_top"] and (game_state["player_y"] + segment_center) < game_state["next_gate_block_bottom"]:
+                blocked = True
+
+            if blocked:
+                inputs[i] = 1
+        
+#        if inputs[1] == 1:
+#            genome.fitness -= 2000
+
         self.genome_index += 1
         if self.genome_index == len(self.population_object.get_population()):
             self.population_object.finish_run()
-            self.population_object.init_run(n=self.generations)
+            self.best = self.population_object.init_run(n=self.generations)
+            print("###################################################################")
+            print(self.best)
+            print("###################################################################")
             self.genome_index = 0
         else:
             genome_id, genome = self.population_object.get_population()[self.genome_index]
@@ -70,7 +104,6 @@ class Agent():
 
             See code for structure.
 
-        """ 
         inputs = list(game_state.values())[1:8]
         inputs[0] /= 500
         inputs[1] /= 50
@@ -79,7 +112,42 @@ class Agent():
         inputs[4] /= 500
         inputs[5] /= 500
         inputs[6] /= 500
+        """
 
+        num_segments = 11
+        inputs = [0] * num_segments
+        sensor_range = 350
+        for i in range(num_segments):
+            
+            segment_top = -(sensor_range / 2.0) + (i * (float(sensor_range) / num_segments))
+            segment_bottom = -(sensor_range / 2.0) + ((i + 1) * (float(sensor_range) / num_segments))
+            segment_center = (segment_top + segment_bottom) / 2.0
+
+            #print(segment_top)
+            #print(segment_bottom)
+            #print(game_state["player_dist_to_floor"])
+            #print(game_state["player_dist_to_ceil"])
+            #print()
+
+            blocked = False
+            #if -game_state["player_dist_to_ceil"] > segment_top:
+            #    blocked = True
+            #elif game_state["player_dist_to_floor"] < segment_bottom:
+            #    blocked = True
+            #elif segment_center < (game_state["player_y"] - game_state["next_gate_block_top"]) and segment_center > (game_state["player_y"] - game_state["next_gate_block_bottom"]):
+                #blocked = True
+
+            if (game_state["player_y"] + segment_center) > game_state["next_gate_block_top"] and (game_state["player_y"] + segment_center) < game_state["next_gate_block_bottom"]:
+                blocked = True
+
+            if blocked:
+                inputs[i] = 1 / (game_state["next_gate_dist_to_player"] / 700)
+
+        inputs.append(0.1 / (game_state["player_dist_to_ceil"] / 400))
+        inputs.append(0.1 / (game_state["player_dist_to_floor"] / 400))
+        inputs.append(game_state["player_vel"] / 50)
+
+        #print(inputs)
 
         action = None
         output = self.current_network.activate(inputs)
